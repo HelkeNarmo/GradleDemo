6945ecd275de40be1f67f1da56a6ed4a2e2b4ddb
zhengjihan <jh9696966@gmail.com>
2019-11-12 10:32:33 +0800

    ci test

--- app/src/main/java/com/narmo/gradledemo/MainActivity.java
+++ app/src/main/java/com/narmo/gradledemo/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ((TextView) findViewById(R.id.txt)).setText(Lalala.class.getName());
+        Toast.makeText(this, "Yeah!", Toast.LENGTH_SHORT).show();
     }
 
 
